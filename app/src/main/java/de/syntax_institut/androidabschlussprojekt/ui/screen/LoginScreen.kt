430d0847d56d2f1752aaa05a37aa3d228d029fca
DilaraOztas <bodur-dilara@hotmail.de>
2025-06-27 09:35:25 +0200

    Ui LoginScreen angepasst

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen/LoginScreen.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen/LoginScreen.kt
@@ -3,8 +3,11 @@ package de.syntax_institut.androidabschlussprojekt.ui.screen
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -17,9 +20,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 import com.facebook.CallbackManager
@@ -39,7 +45,6 @@ fun LoginScreen(
     navController: NavHostController
 ) {
     val context = LocalContext.current
-
     val user by authViewModel.currentUser.collectAsState()
 
     LaunchedEffect(user) {
@@ -49,8 +54,8 @@ fun LoginScreen(
             }
         }
     }
-    val clientId = stringResource(R.string.default_web_client_id)
 
+    val clientId = stringResource(R.string.default_web_client_id)
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -65,42 +70,74 @@ fun LoginScreen(
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF083A8C))
+            .padding(32.dp)
     ) {
-        Button(onClick = { authViewModel.loginAnonym() }) {
-            Text("Anonym einloggen")
-        }
+        Column(
+            modifier = Modifier.fillMaxSize().padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Willkommen bei Pick-it",
+                fontSize = 28.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(12.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Wie möchtest du dich einloggen?",
+                fontSize = 28.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Medium
+            )
 
-        Button(onClick = {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(clientId)
-                .requestEmail()
-                .build()
+            Spacer(modifier = Modifier.height(68.dp))
 
-            val googleSignInClient = GoogleSignIn.getClient(context, gso)
-            val signInIntent = googleSignInClient.signInIntent
-            launcher.launch(signInIntent)
-        }) {
-            Text(text = "Mit Google einloggen")
-        }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = { authViewModel.loginAnonym() }) {
+                    Text("Als Gast")
+                }
 
-        AndroidView(factory = {
-            LoginButton(it).apply {
-                setPermissions("email", "public_profile")
-                registerCallback(CallbackManager.Factory.create(), object : FacebookCallback<LoginResult> {
-                    override fun onSuccess(result: LoginResult) {
-                        authViewModel.loginWithFacebook(result.accessToken)
+                Button(onClick = {
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(clientId)
+                        .requestEmail()
+                        .build()
+
+                    val googleSignInClient = GoogleSignIn.getClient(context, gso)
+                    googleSignInClient.signOut().addOnCompleteListener {
+                        val signInIntent = googleSignInClient.signInIntent
+                        launcher.launch(signInIntent)
                     }
-
-                    override fun onCancel() {}
-                    override fun onError(error: FacebookException) {}
-                })
+                }) {
+                    Text(text = "Mit Google")
+                }
             }
-        })
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            AndroidView(factory = {
+                LoginButton(it).apply {
+                    setPermissions("email", "public_profile")
+                    registerCallback(CallbackManager.Factory.create(), object : FacebookCallback<LoginResult> {
+                        override fun onSuccess(result: LoginResult) {
+                            authViewModel.loginWithFacebook(result.accessToken)
+                        }
+
+                        override fun onCancel() {}
+                        override fun onError(error: FacebookException) {}
+                    })
+                }
+            })
+        }
     }
+
 }
